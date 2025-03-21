c24049fb73e52bfccb238323d25bed021ce178f7
byeon seongun <kamyu1537@gmail.com>
2025-03-21 11:20:09 +0900

    init commit

---
+++ Plugin.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace PunFixedRegion;
+
+[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+public class PunFixedRegionPlugin : BaseUnityPlugin
+{
+    private const string PluginGuid = "PunFixedRegion";
+    private const string PluginName = "PunFixedRegion";
+    private const string PluginVersion = "1.0.0";
+
+    internal new static ManualLogSource Logger;
+    internal static ConfigEntry<bool> Enabled;
+    internal static ConfigEntry<string> Region;
+    
+    private readonly Harmony _harmony = new(PluginGuid);
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {PluginGuid} is loaded!");
+
+        SetupConfiguration();
+
+        _harmony.PatchAll(typeof(SteamManagerPatch));
+        Logger.LogInfo("SteamManagerPatch is patched!");
+    }
+
+    private void SetupConfiguration()
+    {
+        Enabled = Config.Bind(
+            "General",
+            "Enabled",
+            true,
+            "Toggle"
+        );
+        
+        Region = Config.Bind(
+            "General",
+            "Region",
+            "",
+            "Empty is default. (https://doc.photonengine.com/pun/current/connection-and-authentication/regions)"
+        );
+    }
+}
---
+++ SteamManagerPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using Photon.Pun;
+
+namespace PunFixedRegion;
+
+internal class SteamManagerPatch
+{
+    [HarmonyPatch(typeof(SteamManager), nameof(SteamManager.HostLobby))]
+    private static bool Prefix()
+    {
+        if (false == PunFixedRegionPlugin.Enabled.Value) return true;
+        
+        var currentFixedRegion = PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion;
+        PunFixedRegionPlugin.Logger.LogInfo("SteamManager.HostLobby called!");
+        PunFixedRegionPlugin.Logger.LogInfo($"Current FixedRegion: {currentFixedRegion}");
+        
+        if (currentFixedRegion == PunFixedRegionPlugin.Region.Value) return true;
+        PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion = PunFixedRegionPlugin.Region.Value;
+        PunFixedRegionPlugin.Logger.LogInfo($"Photon Fixed Region changed!: {currentFixedRegion} -> {PunFixedRegionPlugin.Region.Value}");
+
+        return true;
+    }
+}
